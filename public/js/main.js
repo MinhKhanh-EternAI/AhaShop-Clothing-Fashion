f09e8c336824cf235ee11a1b1fffb10350e4d011
Karrot <karrottt.666@gmail.com>
2024-01-15 00:23:54 +0700

    done

--- public/js/main.js
+++ public/js/main.js
@@ -29,12 +29,12 @@ function backToTop() {
 /////////////// Thêm sản phẩm ///////////////
 
 const product_ap = [
-    {
-        id_ap: 0,
-        image_ap: './public/image/product-1.jpg',
-        title_ap: 'Minty Dress',
-        price_ap: 100,
-    }
+  {
+    id_ap: 0,
+    image_ap: './public/image/product-1.jpg',
+    title_ap: 'Minty Dress',
+    price_ap: 100,
+  }
 ];
 
 
@@ -46,65 +46,52 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 // Hiển thị sản phẩm
 let i_ap = 0;
 document.getElementById('root').innerHTML = categories_ap.map((item) => {
-    var { image_ap, title_ap, price_ap } = item;
-    return (
-        "<button onclick='addtocart(" + (i_ap++) + ")'>Add to cart</button>"
-    )
+  var { image_ap, title_ap, price_ap } = item;
+  return (
+    "<button onclick='addtocart(" + (i_ap++) + ")'>Add to cart</button>"
+  )
 }).join('')
 
 // Thêm sản phẩm vào giỏ hàng
 function addtocart(a) {
-    cart.push({ ...categories_ap[a] });
-    localStorage.setItem("cart", JSON.stringify(cart)); // Lưu giỏ hàng vào localStorage
-    displaycart();
+  cart.push({ ...categories_ap[a] });
+  localStorage.setItem("cart", JSON.stringify(cart)); // Lưu giỏ hàng vào localStorage
+  displaycart();
 }
 
 // Xóa sản phẩm khỏi giỏ hàng
 function delElement(a) {
-    cart.splice(a, 1);
-    localStorage.setItem("cart", JSON.stringify(cart)); // Lưu giỏ hàng vào localStorage
-    displaycart();
+  cart.splice(a, 1);
+  localStorage.setItem("cart", JSON.stringify(cart)); // Lưu giỏ hàng vào localStorage
+  displaycart();
 }
 
 // Hiển thị giỏ hàng
 function displaycart() {
-    let j_ap = 0, total_ap = 0;
-    document.getElementById("count").innerHTML = cart.length;
-    if (cart.length == 0) {
-        document.getElementById('cartItem').innerHTML = "Your cart is empty.";
-        document.getElementById("total").innerHTML = "$" + 0 + ".00";
-        document.getElementById("total-1").innerHTML = "$" + 0 + ".00";
-    }
-    else {
-        document.getElementById("cartItem").innerHTML = cart.map((items) => {
-            var { image_ap, title_ap, price_ap } = items;
-            total_ap = total_ap + price_ap;
-            document.getElementById("total").innerHTML = "$" + total_ap + ".00";
-            document.getElementById("total-1").innerHTML = "$" + total_ap + ".00";
-            return (
-                `<div class='cart-item'>
+  let j_ap = 0, total_ap = 0;
+  document.getElementById("count").innerHTML = cart.length;
+  if (cart.length == 0) {
+    document.getElementById('cartItem').innerHTML = "Your cart is empty.";
+    document.getElementById("total").innerHTML = "$" + 0 + ".00";
+    document.getElementById("total-1").innerHTML = "$" + 0 + ".00";
+  }
+  else {
+    document.getElementById("cartItem").innerHTML = cart.map((items) => {
+      var { image_ap, title_ap, price_ap } = items;
+      total_ap = total_ap + price_ap;
+      document.getElementById("total").innerHTML = "$" + total_ap + ".00";
+      document.getElementById("total-1").innerHTML = "$" + total_ap + ".00";
+      return (
+        `<div class='cart-item'>
                     <div class='row-img'>
                         <img class='rowimg' src=${image_ap}>
                     </div>
                     <h2>${title_ap}</h2>
                     <p>$ ${price_ap}.00</p>` +
-                    "<i class='fa fa-trash fa-2x' onclick='delElement(" + (j_ap++) + ")'></i></div>"
-            );
-        }).join('');
-    }
+        "<i class='fa fa-trash fa-2x' onclick='delElement(" + (j_ap++) + ")'></i></div>"
+      );
+    }).join('');
+  }
 }
 
 displaycart(); // Hiển thị giỏ hàng khi tải trang
-
-/////////////// Thêm sản phẩm ///////////////
-
-function searchproduct() {
-  let sp = document.getElementById('search-input').innerHTML;
-  for
-}
-
-const list_product = [
-  {
-      title_ap: 'Minty Dress',
-  }
-];
